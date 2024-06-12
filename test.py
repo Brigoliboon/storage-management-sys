016186ee9a722b1198fa1aae6db058939a4d6474
Brigoliboon <95647084+Brigoliboon@users.noreply.github.com>
2024-06-12 12:10:15 +0800

    Update test.py

--- test.py
+++ test.py
@@ -7,12 +7,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
+'''@app.get("/")
 async def root():
     return {"message": "Hello from FastAPI!"}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)'''
 app = FastAPI()
 
 class Item(BaseModel):
